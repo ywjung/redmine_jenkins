21c2d2e8ec1aec94d2ded72aca77668e90442842
Yongwook Jung <ywjung99@naver.com>
2022-04-06 10:57:36 +0900

    Update projects_helper_patch.rb

--- lib/redmine_jenkins/patches/projects_helper_patch.rb
+++ lib/redmine_jenkins/patches/projects_helper_patch.rb
@@ -1,40 +1,23 @@
 require_dependency 'projects_helper'
 
 module RedmineJenkins
-  module Patches
-    module ProjectsHelperPatch
-
-      def self.included(base)
-        base.send(:include, InstanceMethods)
-        base.class_eval do
-          unloadable
-
-          alias_method :project_settings_tabs_without_redmine_jenkins, :project_settings_tabs
-          alias_method :project_settings_tabs, :project_settings_tabs_with_redmine_jenkins
-        end
-      end
-
-
-      module InstanceMethods
-
-        def project_settings_tabs_with_redmine_jenkins(&block)
-          tabs = project_settings_tabs_without_redmine_jenkins(&block)
-          tabs.push({
-            :name    => 'jenkins',
-            :action  => :edit_jenkins_settings,
-            :partial => 'projects/settings/redmine_jenkins',
-            :label   => :label_jenkins
-          })
-          tabs.select {|tab| User.current.allowed_to?(tab[:action], @project)}
-          tabs
-        end
-
-      end
-
+  module ProjectSettingsTabs
+    def self.apply
+      ProjectsController.send :helper, RedmineJenkins::ProjectSettingsTabs
     end
-  end
-end
 
-unless ProjectsHelper.included_modules.include?(RedmineJenkins::Patches::ProjectsHelperPatch)
-  ProjectsHelper.send(:include, RedmineJenkins::Patches::ProjectsHelperPatch)
+    def project_settings_tabs
+      tabs = super
+      if User.current.allowed_to?(:edit_jenkins_settings, @project)
+        tabs.push({
+          name: 'jenkins',
+          action: :edit_jenkins_settings, 
+          partial: 'projects/settings/redmine_jenkins', 
+          label: :label_jenkins
+        })
+      end
+      tabs
+    end
+
+  end
 end
